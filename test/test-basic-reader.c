736a55d72137d70bef2c55808d578122d49cd41a
Simon Howard <fraggle@gmail.com>
2012-03-31 13:04:07 +0100

    Update copyright year.

--- test/test-basic-reader.c
+++ test/test-basic-reader.c
@@ -1,6 +1,6 @@
 /*
 
-Copyright (c) 2011, Simon Howard
+Copyright (c) 2011, 2012, Simon Howard
 
 Permission to use, copy, modify, and/or distribute this software
 for any purpose with or without fee is hereby granted, provided
