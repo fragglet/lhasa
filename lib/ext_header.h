201cb11930e87eaa7ad04973d2c98b4cbb674f95
Simon Howard <fraggle@gmail.com>
2012-05-20 15:36:43 +0100

    Consolidate extended header code into single file.
    Dividing the parsers into separate files by category seems overly
    complicated. Move all the parsers into ext_header.c and make some of the
    definitions private to that file.

--- lib/ext_header.h
+++ lib/ext_header.h
@@ -25,48 +25,6 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 #include "lha_file_header.h"
 
-// Extended header types:
-
-#define LHA_EXT_HEADER_COMMON              0x00
-#define LHA_EXT_HEADER_FILENAME            0x01
-#define LHA_EXT_HEADER_PATH                0x02
-#define LHA_EXT_HEADER_MULTI_DISC          0x39
-#define LHA_EXT_HEADER_COMMENT             0x3f
-
-#define LHA_EXT_HEADER_WINDOWS_TIMESTAMPS  0x41
-
-#define LHA_EXT_HEADER_UNIX_PERMISSION     0x50
-#define LHA_EXT_HEADER_UNIX_UID_GID        0x51
-#define LHA_EXT_HEADER_UNIX_GROUP          0x52
-#define LHA_EXT_HEADER_UNIX_USER           0x53
-#define LHA_EXT_HEADER_UNIX_TIMESTAMP      0x54
-
-/**
- * Callback function for parsing an extended header block.
- *
- * @param header     The file header structure in which to store
- *                   decoded data.
- * @param data       Pointer to the header data to decode.
- * @param data_len   Size of the header data, in bytes.
- * @return           Non-zero if successful, or zero for failure.
- */
-
-typedef int (*LHAExtHeaderParser)(LHAFileHeader *header,
-                                  uint8_t *data,
-                                  size_t data_len);
-
-typedef struct _LHAExtHeaderType LHAExtHeaderType;
-
-/**
- * Structure representing an extended header type.
- */
-
-struct _LHAExtHeaderType {
-	uint8_t num;
-	LHAExtHeaderParser decoder;
-	size_t min_len;
-};
-
 /**
  * Decode the specified extended header.
  *
