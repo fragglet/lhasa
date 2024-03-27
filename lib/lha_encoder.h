beb6cdcd9285e68d7d6bba84b96a6184a3b087e9
Simon Howard <fraggle@soulsphere.org>
2024-03-27 18:59:35 -0400

    Add prototypes for LHAEncoder functions.
    These should eventually be in a public header, but for now we'll just
    use the private header.

--- lib/lha_encoder.h
+++ lib/lha_encoder.h
@@ -21,8 +21,6 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 #ifndef LHASA_LHA_ENCODER_H
 #define LHASA_LHA_ENCODER_H
 
-#include "public/lha_decoder.h"
-
 struct _LHAEncoder {
 
 	/** Type of encoder (algorithm) */
@@ -46,6 +44,73 @@ struct _LHAEncoder {
 	void *callback_data;
 };
 
+// TODO: Much of the text below should be in a public header file.
+
+/**
+ * Opaque type representing an instance of an encoder.
+ *
+ * An encoder is used to compress a stream of data. Instantiated using the @ref
+ * lha_encoder_new function and freed using the @ref lha_encoder_free function.
+ */
 typedef struct _LHAEncoder LHAEncoder;
+
+/**
+ * Allocate a new encoder for the specified compression type.
+ *
+ * @param codec          Codec for compressing data, as returned by
+ *                       @ref lha_encoder_for_name.
+ * @param callback       Callback function for the encoder to call to read more
+ *                       data to compress.
+ * @param callback_data  Extra data to pass to the callback function.
+ * @return               Pointer to the new encoder, or NULL for failure.
+ */
+LHAEncoder *lha_encoder_new(LHACodec *codec,
+                            LHACodecCallback callback,
+                            void *callback_data);
+
+/**
+ * Get the encoder type for the specified name.
+ * @param name        String identifying the compression scheme, for example
+ *                    "-lh1-".
+ * @return            Pointer to the encoder type, or NULL if there is no
+ *                    encoder type with that name.
+ */
+LHACodec *lha_encoder_for_name(char *name);
+
+/**
+ * Free an encoder.
+ *
+ * @param encoder      The encoder.
+ */
+void lha_encoder_free(LHAEncoder *encoder);
+
+/**
+ * Encode (compress) more data.
+ *
+ * @param encoder       The encoder.
+ * @param buf           Pointer to buffer to store compressed data.
+ * @param buf_len       Size of the buffer in bytes.
+ * @return              Number of compressed bytes read.
+ */
+size_t lha_encoder_read(LHAEncoder *encoder, uint8_t *buf, size_t buf_len);
+
+/**
+ * Get the current 16-bit CRC of the uncompressed data.
+ *
+ * This should be called at the end of compression and stored in the file
+ * header.
+ *
+ * @param encoder       The encoder.
+ * @return              16-bit CRC of the data read for compression so far.
+ */
+uint16_t lha_encoder_get_crc(LHAEncoder *encoder);
+
+/**
+ * Get the count of the number of uncompressed bytes read.
+ *
+ * @param encoder       The encoder.
+ * @return              The number of bytes read for compression so far.
+ */
+size_t lha_encoder_get_length(LHAEncoder *encoder);
 
 #endif /* #ifndef LHASA_LHA_ENCODER_H */
